7c8858b58913898a7dcb5d45b48e66bca87c8e03
Johannes Rackles <john@rackles.io>
2019-07-09 13:36:37 +0200

    Iteration

---
+++ iteration/iteration.go
@@ -0,0 +1,9 @@
+package iteration
+
+import "strings"
+
+// Repeat repeats a supplied character n times
+func Repeat(character string, repeatCount int) string {
+
+	return strings.Repeat(character, repeatCount)
+}
